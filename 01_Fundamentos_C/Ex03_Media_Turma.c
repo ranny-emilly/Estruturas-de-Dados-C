86ff17b3b7598accf7bf7b1fd3ba0f6f8b40ec5b
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-29 15:05:49 -0300

    Create Ex03_Media_Turma.c

---
+++ 01_Fundamentos_C/Ex03_Media_Turma.c
@@ -0,0 +1,41 @@
+/*Crie um sistema que peça quantas notas o professor quer digitar. Depois, peça cada nota, calcule a média e diga se a turma foi aprovada.
+Fluxo do programa:
+Pergunte: "Quantos alunos tem na turma?".
+Use um loop (for ou while) para pedir a nota de cada aluno (ex: "Digite a nota do aluno 1: ...").
+Vá somando essas notas numa variável acumuladora.
+No final, calcule a média da turma.
+Pergunte: "Deseja calcular outra turma? (s/n)". Se digitar 's', o programa reinicia (Loop do-while global).*/
+
+#include <stdio.h>
+
+int main(){
+    int alunos, i = 0;
+    float nota, media, soma = 0;
+    char outra;
+
+    do {
+        printf("Quantos alunos tem na turma?\n");
+        scanf("%d", &alunos);
+
+        while(i < alunos){
+        printf("Digite a nota do aluno %d: ", i+1);
+        scanf("%f", &nota);
+        soma += nota;
+        i++;
+        }
+
+    media = soma/alunos;
+    printf("Soma = %.2f / %d alunos = %.2f\n", soma, alunos, media);
+
+    printf("Deseja calcular outra turma? (s/n)\n");
+    scanf(" %c", &outra);
+
+    if(outra == 'n'){
+        printf("Programa encerrado.");
+    }
+
+    } while (outra == 's');
+
+
+    return 0;
+}
