f9be771f63717fc3376a33aa2c7d937473f9353e
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-12-29 12:14:29 -0300

    Add files via upload

---
+++ 03_Ponteiros_e_Memoria/Malloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*memory alloc
+malloc retorna um ponteiro para a região de memória alocada ou NULL*/
+
+int main()
+{
+    int *x;
+    
+    x = malloc(sizeof(int));
+    
+    
+    if(x){
+        printf("Memoria alocada com sucesso\n");
+    }else{
+        printf("Erro ao alocar memoria!\n");
+    }
+
+//Atribuir um valor ao ponteiro/conteúdo apontado por x
+    printf("x: %d\n", *x);
+    *x = 50;
+    printf("x: %d\n", *x);
+
+    free(x);
+    return 0;
+}
