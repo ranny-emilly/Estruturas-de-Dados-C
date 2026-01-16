610f2cdf4dc4f071e8a404470a48f32554435c2c
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-01-16 10:04:20 -0300

    Create listaDE.h

---
+++ 06_Estrutura_Linear_Dinamica/LDE/Headers/listaDE.h
@@ -0,0 +1,15 @@
+#ifndef LISTADE_H
+#define LISTADE_H
+#include <stdbool.h>
+
+typedef struct NoDuplo{
+    int dado;
+    struct NoDuplo *prox;
+    struct NoDuplo *ant;
+}NoDuplo;
+
+
+
+
+#endif 
+
