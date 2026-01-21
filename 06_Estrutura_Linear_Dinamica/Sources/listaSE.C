25d528adeb6e5cd057c82b815a58920905452035
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-01-21 10:52:42 -0300

    Update listaSE.C

--- 06_Estrutura_Linear_Dinamica/Sources/listaSE.C
+++ 06_Estrutura_Linear_Dinamica/Sources/listaSE.C
@@ -71,3 +71,48 @@ bool ls_atualizar_primeiro(NoSimples *cabeca, int antigo, int novo){
 
 }
 
+bool ls_remover_valor(NoSimples **cabeca, int valor){
+    NoSimples *p = *cabeca; //preciso da referencia inicial da lista
+    NoSimples *ant =  NULL; //preciso garantir que inicie em null, mas vai servir para armazenar info do anterior
+    
+    while(p != NULL && p->dado != valor)/*while funciona enquanto p for != nulo ou o dado de remoção for diferente do valor*/{
+        ant = p; 
+        p = p->prox;
+    }
+    if(p == NULL)/*chegou até o final da lista e não encontrou*/{
+        return false;
+    } else if(ant == NULL)/*O novo cabeca passa a ser o segundo da fila.*/{
+        *cabeca = p->prox; /*atualizar a info para dizer que o proximo sera o primeiro (cabeca)*/
+    }
+    else{
+        ant -> prox = p->prox;
+        //1, 2, 3; Supondo que o p é o 2. Antes de remover o 2, preciso apontar o 1 para o 3. Então pega a referência do 2 que aponta para o 3 e passa para o 1.
+    }
+
+    free(p);
+    return true;
+}
+
+void ls_exibir(const NoSimples *cabeca){
+
+    const NoSimples *p = cabeca;/* um ponteiro auxiliar 'p' que recebe o endereço do início.
+       O 'const' é um "cadeado": ele garante que a função ls_exibir
+       NÃO PODE alterar os dados da lista. Ela só pode OLHAR (read-only).
+    */
+
+    while(p != NULL){
+        prinf("%d -> \t", p->dado);
+        p = p->prox;
+    }
+    printf("NULL. \n");
+}
+
+void ls_limpar(NoSimples **cabeca){
+    NoSimples *p = *cabeca;
+    while(p!= NULL){
+        NoSimples *prox = p ->prox;
+        free(p);
+        p = prox;
+    }
+    *cabeca = NULL;
+}
