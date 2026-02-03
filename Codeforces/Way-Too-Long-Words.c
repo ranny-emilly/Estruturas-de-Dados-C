d3f32f71656a68c0042d7e35e414345a31400fb6
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-02-02 22:03:08 -0300

    Create Way-Too-Long-Words.c

---
+++ Codeforces/Way-Too-Long-Words.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+   int n, tamanho;
+
+   scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        char s[];
+        tamanho = strlen(s);
+
+
+
+    }
+
+
+    return 0;
+}
