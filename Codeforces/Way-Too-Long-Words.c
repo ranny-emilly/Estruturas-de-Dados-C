76c34fc187fdbb098ede800f6adc517e7f3c0629
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-04-03 20:01:56 -0300

    Update Way-Too-Long-Words.c

--- Codeforces/Way-Too-Long-Words.c
+++ Codeforces/Way-Too-Long-Words.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(){
-   int n, tamanho;
+   int n, tamanho = 0;
+   char s[100];
 
    scanf("%d", &n);
     for(int i = 0; i < n; i++){
-        char s[];
+        scanf("%s", s[i]);
         tamanho = strlen(s);
-
-
+            if(tamanho > 10){
+                printf("%c%d%c", s[0], tamanho, s[100]);
+                return 0;
+            }
 
     }
 
